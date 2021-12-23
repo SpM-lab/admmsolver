196537e50021f9929eb045e1a4f08cab5acda345
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-12-23 10:03:29 +0900

    Implementing main objective functions

---
+++ setup.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2021-2021 H. Shinaoka and others
+# SPDX-License-Identifier: MIT
+import io, os.path, re
+from setuptools import setup, find_packages
+
+def readfile(*parts):
+    """Return contents of file with path relative to script directory"""
+    herepath = os.path.abspath(os.path.dirname(__file__))
+    fullpath = os.path.join(herepath, *parts)
+    with io.open(fullpath, 'r') as f:
+        return f.read()
+
+
+def extract_version(*parts):
+    """Extract value of __version__ variable by parsing python script"""
+    initfile = readfile(*parts)
+    version_re = re.compile(r"(?m)^__version__\s*=\s*['\"]([^'\"]*)['\"]")
+    match = version_re.search(initfile)
+    return match.group(1)
+
+
+VERSION = extract_version('src', 'admmsolver', '__init__.py')
+REPO_URL = "https://github.com/shinaoka/admmsolver"
+LONG_DESCRIPTION = readfile('README.md')
+
+setup(
+    name='admmsolver',
+    version=VERSION,
+
+    description=
+        'Fast and general ADMM solver',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=' '.join([
+        'ADMM'
+        ]),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        ],
+
+    url=REPO_URL,
+    author=[
+        'Hiroshi Shinaoka',
+        ],
+    author_email='h.shinaoka@gmail.com',
+
+    python_requires='>=3',
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+    extras_require={
+        'test': ['pytest'],
+        },
+
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    )
---
+++ src/admmsolver/objective_functions.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+
+class MainLestSquaresBase(object):
+    """
+    Base class for main least-squares fitting problem with linear constraint
+        |y - A x|_2^2
+    subject to the constraint V x = 1.
+    The shares of these matrices/vectors are
+        y: (Ny,)
+        A: (Ny, Nx)
+        V: (Nc, Nx)
+    The constraint is tread by the method of Lagrange multiplier.
+    We optimize
+        |y - A x|_2^2 - nu^+ (V x -1) - (V x - 1)^+ nu
+    """
+    def __init__(self, A, y, V):
+        assert y.ndim == 1
+        assert y.ndim == 2
+        self._Ny, self._Nx = A.shape
+        self._Nc = V.shape[0]
+        assert y.shape == (self._Ny,)
+        assert V.shape == (self._Nc, self._Nx)
+
+        self._A = A
+        self._y = y
+        self._V = V
+    
+    def solve_for_x(self, nu, h, mu):
+        """
+        Return new x
+         = argmin_x |y - Ax|_2^2 + h^+ x + x^+ h + mu x^+ x
+        """
+        pass
+    
+    def solve_for_nu(self, y, nu, h, mu):
+        """
+        Return new nu
+
+        xi1 = (A^+ A + mu I)^{-1} (A^+ y - h + V^+ nu)
+        xi2 = (A^+ A + mu I)^{-1} V
+        nu <- (V xi2)^{-1} (1 - V xi1)
+        """
+        pass
+
+
+class DenseMainLestSquares(MainLestSquaresBase):
+    """
+    Main least-squares fitting problem with a dense matrix A
+    """
+    def __init__(self, A, y, V):
+        assert A.ndim == 2
+        super().__(A, y, V)
+
+        self._Ac = A.conjugate().T
+        self._AcA = A.conjugate().T @ A
+
+        # B = (A^+ A + mu I)^{-1}
+        self._B_cache = (None, None)
+    
+    
+    def _get_B(self, mu):
+        assert mu>=0
+        if self._B_cache[0] != mu:
+            self._B_cache = (
+                mu,
+                np.inv(self._AcA + mu * np.identity(A.shape[0]))
+            )
+        return self._B_cache[1]
+    
+    def solve_for_x(self, nu, h, mu):
+        """
+        Return new x
+        """
+        return self._get_B(mu) @ (self._Ac @ self._y - self._h)
+
+    def solve_for_nu(self, y, nu, h, mu):
+        """
+        Return new nu
+        """
+        B = self._get_B(mu)
+        xi1 = B @ (self._Ac @ y - h + self._V.conjugate().T @ nu)
+        xi2 = B @ V
+        return np.inv(V @ xi2) @ (np.identity(self._Nc) - self._V @ xi1)
+
+
+class SparseMainLestSquares(MainLestSquaresBase):
+    """
+    Main least-squares fitting problem with a sparse matrix A
+    """
+    pass
+
+
+class DiagonalMainLestSquares(MainLestSquaresBase):
+    """
+    Main least-squares fitting problem with a diagonal matrix A
+    """
+    pass
