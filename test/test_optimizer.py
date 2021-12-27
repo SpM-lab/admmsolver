7e9a6746bf691a71d097d62cdd0999236a8ebc23
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-12-27 16:33:09 +0900

    Some clean up

--- test/test_optimizer.py
+++ test/test_optimizer.py
@@ -14,7 +14,7 @@ def test_LASSO():
        A = [[2., 1.]]
        alpha = 0.1
       
-    The solution is x \simeq (1, 0).
+    The solution is x simeq (1, 0).
 
     This problem can be solved by minimizing
        |y - A * x0|^2 + alpha |x1|_1
@@ -44,4 +44,33 @@ def test_LASSO():
     for x in x_res:
         np.testing.assert_allclose(x, x_ref, atol=1e-10)
 
-    
+def test_basis_pursuit():
+    # Fig. 3 of J. Otsuki et al., JPSJ89, 012001 (2020)
+    # https://github.com/SpM-lab/CS-tools/tree/master/jpsj-review/basis_pursuit
+    # Dimension of the signal
+    N = 1000
+
+    M = 100
+    K = 20    
+    seed = 1234
+    np.random.seed(seed)
+    A = np.random.randn(M,N)
+   
+    #Make answer vector
+    xanswer = np.zeros(N)
+    xanswer[:K] = np.random.randn(K)
+    xanswer = np.random.permutation(xanswer)
+   
+    y_calc = np.dot(A, xanswer)    
+
+    lstsq = LeastSquares(1.0, A, y_calc)
+    l1 = L1Regularizer(1e-1, A.shape[1])
+    equality_conditions = [
+      (1, 0, identity(N), identity(N), 1.0, None)
+    ]
+    opt = SimpleOptimizer([lstsq, l1], equality_conditions)
+
+    niter = 100
+    opt.solve(niter)
+
+    np.testing.assert_allclose(opt.x[0], xanswer, atol=1e-2*np.abs(xanswer).max(), rtol=0)
